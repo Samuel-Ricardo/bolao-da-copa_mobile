762b581c5c1c2a24a67c8cb5b009b046e2293341
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-13 18:15:08 -0300

    :sparkles: create: screen - Signin (src/screens)

---
+++ src/screens/Signin.tsx
@@ -0,0 +1,9 @@
+import { Center, Text, StatusBar } from 'native-base';
+export function Signin(){
+    return (
+        <Center flex={1} bgColor="black">
+            <Text >Hello World :D!</Text>
+            <StatusBar/>
+        </Center>
+    )
+}
