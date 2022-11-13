5a645719c0d5daeec4dedf5d05a9c8037ec715ec
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-13 18:22:42 -0300

    :lipstick: load: theme

--- App.tsx
+++ App.tsx
@@ -2,13 +2,14 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@
 import { NativeBaseProvider } from "native-base";
 import { Loading } from './src/components/Loading';
 import { Signin } from "./src/screens";
+import { THEME } from "./src/styles/theme";
 
 export default function App() {
 
   const [loaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold})
 
 return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       {!loaded? <Loading/> : <Signin/>}
     </NativeBaseProvider>
   );
