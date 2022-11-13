8e3807d25eee4f5c444e66a0df8de32eb07725c6
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-13 17:56:37 -0300

    :lipstick: create: themes - app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider } from 'native-base';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from "native-base";
+import { StatusBar } from "native-base/lib/typescript/components/basic";
+import { Center } from "native-base/lib/typescript/components/composites";
+import { Text } from "native-base/lib/typescript/components/primitives";
 
 export default function App() {
-  return (
+return (
     <NativeBaseProvider>
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello World :D!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Center flex={1} bgColor="black">
+        <Text >Hello World :D!</Text>
+        <StatusBar/>
+      </Center>
     </NativeBaseProvider>
   );
 }
