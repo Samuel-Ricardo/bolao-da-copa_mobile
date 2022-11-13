8679d68cbb25d47eeb48b6f6416ea8424d35a276
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-13 18:15:03 -0300

    :sparkles: create: screen - Signin (src/screens)

--- App.tsx
+++ App.tsx
@@ -6,10 +6,7 @@ import { Text } from "native-base/lib/typescript/components/primitives";
 export default function App() {
 return (
     <NativeBaseProvider>
-      <Center flex={1} bgColor="black">
-        <Text >Hello World :D!</Text>
-        <StatusBar/>
-      </Center>
+      
     </NativeBaseProvider>
   );
 }
