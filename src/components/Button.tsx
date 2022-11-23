c2515f3e18ef108c1f0137c75903ff73581484a4
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-22 21:33:24 -0300

    :sparkles: create: component - Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,14 @@
-import { ButtonProps } from "react-native";
+import { Button as ButtonNativeBase, IButtonProps, Text } from "native-base";
 
-interface IProps extends ButtonProps {
+interface IProps extends IButtonProps {
     title: string;
     type?: 'PRIMARY' | 'SECONDARY';
 }
 
+export function Button({title, type='PRIMARY', ...rest}: IProps) {
+    return (
+        <ButtonNativeBase>
+            {title}
+        </ButtonNativeBase>
+    )
+}
