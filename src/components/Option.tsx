a43884897c38467fd26705fc614323063047d47d
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-22 22:23:45 -0300

    :sparkles: create: component - Options

--- src/components/Option.tsx
+++ src/components/Option.tsx
@@ -1,7 +1,18 @@
-import { IPressableProps } from "native-base";
+import { Center, IPressableProps, Pressable, Text } from "native-base";
 
-interface Iprops extends IPressableProps {
-    titel: string;
-    isSelectable: boolean;
+interface IProps extends IPressableProps {
+    title: string;
+    isSelected: boolean;
 }
 
+export function Options({title, isSelected = false, ...rest}:IProps) {
+    return(
+        <Pressable flex={1} h={7} maxH={7} {...rest}>
+            <Center h="full" w="full" bgColor={isSelected ? "gray.600" : "transparent"} rounded="sm">
+                <Text color="gray.100" fontFamily="heading" fontSize="xs">
+                    {title}
+                </Text>
+            </Center>
+        </Pressable>
+    )
+}
