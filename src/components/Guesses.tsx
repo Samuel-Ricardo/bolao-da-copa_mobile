9e8b88c2ff13a769955715526f8ccb17b851fd60
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-27 12:23:14 -0300

    :sparkles: create: fun - fetchGame

--- src/components/Guesses.tsx
+++ src/components/Guesses.tsx
@@ -10,5 +10,25 @@ export function Guesses(poolId: string) {
     const [firstTeamPoints, setFirstTeamPoints] = useState('')
     const [secondTeamPoints, setSecondTeamPoints] = useState('')
     
-    
+    const toast = useToast();
+
+    async function fetchGames() {
+        try {
+            setIsLoading(true)
+
+            const response = await api.get(`/pools/${poolId}/games`)
+            setGames(response.data.games);
+            console.log(response.data.games);
+        } catch(err) {
+            toast.show({
+                title: "Não foi possível listar os jogos :/",
+                placement: 'top',
+                bgColor: 'red.500'
+            });
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+
 }
