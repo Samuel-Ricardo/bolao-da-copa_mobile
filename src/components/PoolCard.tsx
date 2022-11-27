5e91a988ce7700bddb9b295e2acc47111d42e79d
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-27 13:04:52 -0300

    :sparkles: create: component - PoolCard

--- src/components/PoolCard.tsx
+++ src/components/PoolCard.tsx
@@ -1,5 +1,6 @@
-import { TouchableOpacityProps } from "react-native";
-import { IParticipantsProps } from "./Participants";
+import { Heading, HStack, Text, VStack } from "native-base";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { IParticipantsProps, Participants } from "./Participants";
 
 export interface IPoolProps {
     id: string;
@@ -19,3 +20,35 @@ export interface IPoolProps {
 interface IProps extends TouchableOpacityProps {
     data: IPoolProps;
 }
+
+export function PoolCard({ data, ...rest }: IProps) {
+    return (
+      <TouchableOpacity {...rest}>
+        <HStack
+          w="full"
+          h={20}
+          bgColor="gray.800"
+          borderBottomWidth={3}
+          borderBottomColor="yellow.500"
+          justifyContent="space-between"
+          alignItems="center"
+          rounded="sm"
+          mb={3}
+          p={4}
+        >
+            <VStack>
+                <Heading color="white" fontSize="md" fontFamily="heading">
+                    {data.title}
+                </Heading>
+
+                <Text color="gray.200" fontSize="xs">
+                    Criado por {data.owner.name}
+                </Text>
+            </VStack>
+                <Participants
+                    count={data._count.participants}
+                    participants={data.participants}
+                />
+        </HStack>
+      </TouchableOpacity>
+)}
