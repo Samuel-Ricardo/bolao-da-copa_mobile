c7f432928cafc53e11b78650ac7a6e4d445788ce
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-26 23:01:26 -0300

    :sparkles: create: component - Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,34 @@
+import { Box, HStack, Text } from "native-base";
+import { CaretLeft } from "phosphor-react-native";
+import Export from "phosphor-react-native/lib/typescript/icons/Export";
+import { ButtonIcon } from './ButtonIcon';
+
 interface IProps {
-    titel: string;
+    title: string;
     showBackButton?: boolean;
     showShareButton?: boolean;
 }
 
+export function Header({ title, showBackButton, showShareButton}: IProps){
+    const EmptyBoxSpace  = () => (<Box w={6} h={6}/>)
+
+    return (
+        <HStack w="full" bgColor="gray.800" alignItems="flex-center" pb={5} px={5}>
+            <HStack w="full" alignItems="center" justifyContent="space-between">
+                {
+                    showBackButton ? <ButtonIcon icon={CaretLeft}/>
+                    : <EmptyBoxSpace/>
+                }
+
+                <Text color="white" fontFamily="medium" fontSize="md" textAlign="center">
+                    {title}
+                </Text>
+            
+                {
+                    showShareButton? <ButtonIcon icon={Export}/>
+                    : <EmptyBoxSpace/> 
+                }
+            </HStack>
+        </HStack>
+    )
+}
