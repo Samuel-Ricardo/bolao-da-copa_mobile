257a137b9489d73b342f412c932c670312943b8a
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-11-27 11:03:17 -0300

    :sparkles: create: interface: IGameProps (.../components/Game.tsx)

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,8 +1,15 @@
-interface GuessProps {
+interface IGuessProps {
     id: string;
     gameId: string;
     createdAt: string;
     participantId: string;
     firstTeamPoints: number;
     secondTeamPoints: number;
+}
+
+interface IGameProps {
+    id: string;
+    firstTeamCountryCode: string;
+    secondTeamCountryCode: string;
+    guess: null | IGuessProps;
 }
