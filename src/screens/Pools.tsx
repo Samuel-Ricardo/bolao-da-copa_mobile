38803604c15783bf65d4f27735bc04d7c7e58d75
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-02-12 17:56:09 -0300

    :lipstick: update: style

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -5,7 +5,7 @@ import {Octicons} from '@expo/vector-icons'
 export const Pools = _ => (
     <VStack flex={1} bgColor="gray.900">
         <Header title="MEUS BOLÕES"/>
-        <VStack>
+        <VStack  mt={6} mx={5} borderBottomWidth={1} borderBottomColor="gray.600" pb={4} mb={4}>
             <Button 
                 title="BUSCAR BOLÃO POR CÓDIGO" 
                 leftIcon={<Icon as={Octicons} name="search" color="black" size='md'/>}    
