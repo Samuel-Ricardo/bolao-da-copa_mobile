f80182820419259bee18c1ae2493c24caf510e62
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-02-13 21:46:00 -0300

    :label: create: type - RootParamList

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,10 @@
+export declare global {
+    namespace ReactNativegation {
+        interface RootParamList {
+            new: undefined;
+            pools: undefined;
+            find: undefined;
+            details: {id:string}
+        }
+    }
+}
