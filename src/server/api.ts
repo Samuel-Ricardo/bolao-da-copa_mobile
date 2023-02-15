ca8dfb8319d2389dddd33b2517600360d156a32b
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-02-15 11:41:12 -0300

    :closed_lock_with_key: use: secret - BASE_URL

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,4 +1,4 @@
+import { BASE_URL } from '@env';
 import axios from 'axios';
-export const api = axios.create({
-    baseURL: 'http://localhost:3333'
-})
+
+export const api = axios.create({ baseURL: BASE_URL })
