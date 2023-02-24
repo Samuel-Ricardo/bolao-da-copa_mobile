778a296c52d028c2996f282eace4a3d5d5df30b1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-02-23 23:13:40 -0300

    :sparkles: implements: navigation - component (EmptyPoolList)

--- src/components/EmptyPoolList.tsx
+++ src/components/EmptyPoolList.tsx
@@ -1,13 +1,18 @@
+import { useNavigation } from "@react-navigation/native";
+import { SCREENS } from '../config'
 import { Pressable, Row, Text } from "native-base";
 
 export function EmptyPoolList() {
+
+  const { navigate } = useNavigation();
+
     return (
         <Row flexWrap="wrap" justifyContent="center">
             <Text color="white" fontSize="sm" textAlign="center">
                 Você ainda não está participando de {'\n'} nenhum bolão, que tal 
             </Text>
 
-            <Pressable>
+            <Pressable onPress={() => navigate(SCREENS.FIND as never)}>
                 <Text>
                     Buscar um pelo código
                 </Text>
@@ -17,7 +22,7 @@ export function EmptyPoolList() {
                 ou
             </Text>
 
-            <Pressable>
+            <Pressable onPress={() => navigate(SCREENS.NEW as never)}>
                 <Text textDecoration="underline" color="yellow.500">
                     criar um novo
                 </Text>
