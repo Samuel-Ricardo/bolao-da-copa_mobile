1fcedb725861154eba5d6dd3639dea5f191d2337
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-02-23 23:57:12 -0300

    :sparkles: setup: screen - Details

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,8 +1,22 @@
-import { VStack } from "native-base";
+import { useRoute } from "@react-navigation/native";
+import { useToast, VStack } from "native-base";
+import { useState } from "react";
+import { IPoolCardProps } from "../components";
 
 interface IRouteParams { id: string }
 
 export function Details () {
+ 
+  const [isLoading, setIsLoading] = useState(true)
+  const [poolDetails, setPoolDetails] = useState<IPoolCardProps>({} as IPoolCardProps)
+
+  const route = useRoute();
+  const toast = useToast();
+
+  const {id} = route.params as IRouteParams;
+  
+
+
   return (
     <VStack>
 
