33f5514471bc7335d1a7d1c22b3fbc2807485afd
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-03 14:59:36 -0300

    :sparkles: create: fun - Error (utils)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,8 @@ import { Share } from "react-native";
 export async function handleCodeShare(code: string) {
     await Share.share({ message: code })
 }
+
+export async function Error(error: any) {
+    console.log(error)
+    throw error
+}
